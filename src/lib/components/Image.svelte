c3d9b4edec39665e872089a400d753e2da2316b9
alonr619 <alan.ragoler@gmail.com>
2022-04-05 16:31:47 -0700

    Image component
    creds to Shak for component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let url;
+    export let alt;
+    export let height = "";
+    export let width = "";
+</script>
+
+{#if height === "" && width === ""}
+<img src={url} alt={alt} />
+{:else if height === "" }
+<img src={url} alt={alt} width={width} />
+{:else if width === ""}
+<img src={url} alt={alt} height={height} />
+{:else}
+<img src={url} alt={alt} width={width} height={height} />
+{/if}
