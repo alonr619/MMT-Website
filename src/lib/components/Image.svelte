7a1b526365fcf10f8970172e2fd09b0c88078f2f
Shak <shak.ragoler@gmail.com>
2022-04-04 23:24:31 -0700

    image components
    image components

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let url;
+    export let alt;
+    export let height = "";
+    export let width = "";
+</script>
+
+{#if height === "" && width === ""}
+<img url={url} alt={alt} />
+{:else if height === "" }
+<img url={url} alt={alt} width={width} />
+{:else if width === ""}
+<img url={url} alt={alt} height={height} />
+{:else}
+<img url={url} alt={alt} width={width} height={height} />
+{/if}
