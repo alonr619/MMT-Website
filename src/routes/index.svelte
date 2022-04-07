7bfd21bc50374e12fec22f2436ea01b9d0fc1c45
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-04-06 23:34:59 -0700

    make register a button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -28,15 +28,13 @@
 
 <Section>
 	<Heading text="Mustang Math Tournament" size={4} textColor="#177012" />
-    <Box color="inherit" bgcolor="#1B9AAA">
-        <Heading text="Register for MMT 2022" />
-    </Box>
+    <Button text="Register for MMT 2022" backgroundColor="#1B9AAA" textColor="white" borderRadius=2 />
 	<Heading text="MMT 2022 on May 28!" size={2} textColor="#177012" />
 </Section>
 <hr />
 
 <Section>
-    <Heading text="Mission Statement Header" size={2} textColor="#1B9AAA" />
+    <Heading text="Mission Statement Header" size={2} textColor="#1B9AAA" alignment="right" />
     <Text text="Mission Statement" />
     <Heading text="Sponsors" size={2} textColor="#1B9AAA" />
     <!-- sponsor stuff will go here -->
