6463fe1c9533183b82120d94ace7d41b58017566
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-04-06 22:43:04 -0700

    Added background color prop

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,7 +1,8 @@
 <script>
     export let color="black";
+    export let bgcolor="inherit";
 </script>
 
-<div style="padding: 10px; border: 2px solid {color}; margin: 50px;">
+<div style="padding: 10px; border: 2px solid {color}; margin: 50px; background-color: {bgcolor}">
     <slot></slot>
 </div>
