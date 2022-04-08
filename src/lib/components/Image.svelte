151b8a62ade7811d1e6d13b268f8177b50b81e6f
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-04-07 21:48:21 -0700

    Img link prop

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,16 +1,43 @@
 <script>
     export let url;
     export let alt;
+    export let link = "";
     export let height = "";
     export let width = "";
 </script>
 
-{#if height === "" && width === ""}
-<img src={url} alt={alt} />
-{:else if height === "" }
-<img src={url} alt={alt} width={width} />
-{:else if width === ""}
-<img src={url} alt={alt} height={height} />
+<!-- Warning: spaghetti code -->
+
+{#if link === ""}
+
+    {#if height === "" && width === ""}
+        <img src={url} alt={alt} />
+    {:else if height === "" }
+        <img src={url} alt={alt} width={width} />
+    {:else if width === ""}
+        <img src={url} alt={alt} height={height} />
+    {:else}
+        <img src={url} alt={alt} width={width} height={height} />
+    {/if}
+
 {:else}
-<img src={url} alt={alt} width={width} height={height} />
+
+    {#if height === "" && width === ""}
+        <a href={link}>
+        <img src={url} alt={alt} />
+        </a>
+    {:else if height === "" }
+        <a href={link}>
+        <img src={url} alt={alt} width={width} />
+        </a>
+    {:else if width === ""}
+        <a href={link}>
+        <img src={url} alt={alt} height={height} />
+        </a>
+    {:else}
+        <a href={link}>
+        <img src={url} alt={alt} width={width} height={height} />
+        </a>
+    {/if}
+
 {/if}
