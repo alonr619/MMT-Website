8c9d57773e107e490ef80a067034754bfa7330fa
Shak <shak.ragoler@gmail.com>
2022-04-07 19:43:48 -0700

    building framework of the archive page
    building framework of the archive page

--- src/lib/components/FlexBox.svelte
+++ src/lib/components/FlexBox.svelte
@@ -4,13 +4,20 @@
     export let wrap = true;
     export let align = "center";
     export let justify = "center";
-
+    export let indent;
+    export let size;
+    
     let alignMap = {start: 'flex-start', center: 'center', end: 'flex-end', stretch: 'stretch'};
     let justifyMap = {start: 'flex-start', center: 'center', end: 'flex-end', around: 'space-around', between: 'space-between', evenly: 'space-evenly'};
     let finalDirection = reverse ? 'reverse-' + direction : direction;
     let finalWrap = wrap ? 'wrap' : 'nowrap';
+
+    function nullFunction(){
+        let x = 0;
+    }
+    export let onClick = nullFunction;
 </script>
 
-<div style="display: flex; flex-direction: {finalDirection}; flex-wrap: {finalWrap}; align-items: {alignMap[align]}; justify-content: {justifyMap[justify]};">
+<div style="display: flex; flex-direction: {finalDirection}; flex-wrap: {finalWrap}; align-items: {alignMap[align]}; justify-content: {justifyMap[justify]}; padding-left: {indent}; font-size: {size}" on:click={onClick}>
     <slot></slot>
 </div>
