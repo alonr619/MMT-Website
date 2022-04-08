4122626e7790e84586683a360c61793256dd7e49
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-04-07 22:47:42 -0700

    javscript object sadge

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,47 +14,96 @@
 	import Image from '$lib/components/Image.svelte';
 	import Box from '$lib/components/Box.svelte';
 	import Section from '$lib/components/Section.svelte';
-    let sponsorTiers = ["Stallion", "Colt", "Foal"]; // Name of sponsor tiers from best to worst
+    let sponsorTiers = [
+            {
+                    "name": "Stallion",
+                    "sponsors": [
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                    ]
+            },
+            {
+                    "name": "Colt",
+                    "sponsors": [
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                    ]
+            },
+            {
+                    "name": "Foal",
+                    "sponsors": [
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                            {
+                                    "url": "about-us-images/alon-ragoler.png",
+                                    "alt": "tech carry",
+                                    "link": "https://cia.gov"
+                            },
+                    ]
+            }
+    ];
 </script>
 
 <svelte:head>
 	<title>Mustang Math Tournament</title>
 </svelte:head>
 
-<Section>
-	<Heading text="Mustang Math Tournament" size={4} textColor="#177012" />
-    <Button text="Register for MMT 2022" backgroundColor="#1B9AAA" textColor="white" borderRadius=2 />
-	<Heading text="MMT 2022 on May 28!" size={2} textColor="#177012" />
-</Section>
-<hr />
+<div style="margin-left:2%; margin-right:2%">
+    <Section>
+    	<Heading text="Mustang Math Tournament" size={4} textColor="#177012" />
+        <Button text="Register for MMT 2022" backgroundColor="#1B9AAA" textColor="white" borderRadius=2 />
+    	<Heading text="MMT 2022 on May 28!" size={2} textColor="#177012" />
+    </Section>
+    <hr style="color: #E58F65"/>
+    
+    <Section>
+        <Heading text="Mission Statement Header" size={2} textColor="#1B9AAA" />
+        <Text text="Mission Statement" />
+        <Heading text="Sponsors" size={2} textColor="#1B9AAA" />
+        {#each sponsorTiers as lvl}
+                <Section>
+                        <Heading text="{lvl.name} Sponsors" size={1.5} />
+                        <FlexBox wrap={true}>
+                                {#each lvl.sponsors as sponsor}
+                                        <Image url={sponsor.url} alt={sponsor.alt} link={sponsor.link} />
+                                {/each}
+                        </FlexBox>
+                </Section>
 
-<Section>
-    <Heading text="Mission Statement Header" size={2} textColor="#1B9AAA" />
-    <Text text="Mission Statement" />
-    <Heading text="Sponsors" size={2} textColor="#1B9AAA" />
-    <Section>
-        <Heading text="{sponsorTiers[0]} Sponsors" size={1.5} />
-        <FlexBox wrap={true}>
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-        </FlexBox>
+        {/each}
     </Section>
-    <Section>
-        <Heading text="{sponsorTiers[1]} Sponsors" size={1.5} />
-        <FlexBox wrap={true}>
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-        </FlexBox>
-    </Section>
-    <Section>
-        <Heading text="{sponsorTiers[2]} Sponsors" size={1.5} />
-        <FlexBox wrap={true}>
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-                <Image url="about-us-images/alon-ragoler.png" alt="tech carry" link="https://cia.gov" />
-        </FlexBox>
-    </Section>
-    <Heading text="Subscribe to our Newsletter" size={2} textColor="#1B9AAA" />
-</Section>
+</div>
