369c2c4dcafe986ce561a2707d53f9eae4baac76
Vince7778 <realvince7778@gmail.com>
2022-04-20 19:52:19 -0700

    Add fade in animation for titles

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,13 +1,14 @@
 <script>
     export let title;
     export let description;
+    export let fadeIn = true;
 
     import Heading from '$lib/components/Heading.svelte';
     import Text from '$lib/components/Text.svelte';
     import Link from '$lib/components/Link.svelte';
 </script>
 
-<div class="header flex"><div>
+<div class="header flex"><div class="{fadeIn ? "fade-down" : ""}">
     <Heading className="glow" text={title} textColor="white" />
     <div class="flex"><div class="headerline"></div></div>
     <p style="font-weight: 300; margin-top: 30px; font-size: 22px; color: white;">{description}</p>
@@ -15,5 +16,18 @@
 </div></div>
 
 <style>
-    
+    .fade-down {
+        animation: fade-down-kf 0.7s;
+    }
+
+    @keyframes fade-down-kf {
+        from {
+            transform: translateY(-20px);
+            opacity: 0;
+        }
+        to {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
 </style>
