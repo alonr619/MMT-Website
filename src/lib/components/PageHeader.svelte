29c97b800cd04971e37d53c842958991cf2d30a3
Vince7778 <realvince7778@gmail.com>
2022-04-20 20:05:22 -0700

    Change header transition to built-in

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -6,9 +6,10 @@
     import Heading from '$lib/components/Heading.svelte';
     import Text from '$lib/components/Text.svelte';
     import Link from '$lib/components/Link.svelte';
+    import { fly } from "svelte/transition";
 </script>
 
-<div class="header flex"><div class="{fadeIn ? "fade-down" : ""}">
+<div class="header flex"><div in:fly="{{ y: -20, duration: 700 }}">
     <Heading className="glow" text={title} textColor="white" />
     <div class="flex"><div class="headerline"></div></div>
     <p style="font-weight: 300; margin-top: 30px; font-size: 22px; color: white;">{description}</p>
@@ -16,18 +17,5 @@
 </div></div>
 
 <style>
-    .fade-down {
-        animation: fade-down-kf 0.7s;
-    }
 
-    @keyframes fade-down-kf {
-        from {
-            transform: translateY(-20px);
-            opacity: 0;
-        }
-        to {
-            transform: translateY(0);
-            opacity: 1;
-        }
-    }
 </style>
