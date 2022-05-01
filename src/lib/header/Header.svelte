6fe050b430b720213294a8c0770e272c37bb9833
Vince7778 <realvince7778@gmail.com>
2022-05-01 15:59:32 -0700

    Rename route as well

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,7 +16,7 @@
         {path: "/", text: "Home"},
         {path: "/mmt-2022", text: "MMT 2022"},
         {path: "/our-team", text: "Our Team"},
-        {path: "/archive", text: "Past Exams"}
+        {path: "/past-exams", text: "Past Exams"}
     ]
 </script>
 
