2890ac5bed4238a9152e32c9ce4eab694cd77088
Vince7778 <realvince7778@gmail.com>
2022-05-01 16:01:47 -0700

    Change page content too

--- src/routes/past-exams.svelte
+++ src/routes/past-exams.svelte
@@ -8,10 +8,10 @@
 </script>
 
 <svelte:head>
-	<title>Archive</title>
+	<title>Past Exams</title>
 </svelte:head>
 
-<PageHeader title="Archive" description="CHECK OUT PAST PROBLEMS" />
+<PageHeader title="Past Exams" description="CHECK OUT PAST PROBLEMS" />
 <section>
     <br />
     <DropdownArchive year="2021" tests={[["Mounting Mayhem", "archive/2021/MountingMayhem.pdf"], ["Bucking Bingo", "archive/2021/BuckingBingo.pdf"], ["Gallop Set 1", "archive/2021/Gallop Set 1.pdf"], ["Gallop Set 2", "archive/2021/Gallop Set 2.pdf"], ["Gallop Set 3", "archive/2021/Gallop Set 3.pdf"], ["Gallop Set 4", "archive/2021/Gallop Set 4.pdf"], ["Gallop Set 5", "archive/2021/Gallop Set 5.pdf"], ["Gallop Set 6", "archive/2021/Gallop Set 6.pdf"], ["Gallop Set 7", "archive/2021/Gallop Set 7.pdf"], ["Gallop Set 8", "archive/2021/Gallop Set 8.pdf"]]} answers="archive/2021/Answers.pdf" results="archive/2021/Results.pdf" />
