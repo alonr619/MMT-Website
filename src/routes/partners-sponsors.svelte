ad0fc951f1e829c077a223a1c15a2076ca37520c
Vince7778 <realvince7778@gmail.com>
2022-05-08 22:17:13 -0700

    Add description text and make links full height

--- src/routes/partners-sponsors.svelte
+++ src/routes/partners-sponsors.svelte
@@ -5,6 +5,7 @@
     import FlexBox from '$lib/components/FlexBox.svelte';
     import Image from '$lib/components/Image.svelte';
     import PanelBox from '$lib/components/PanelBox.svelte';
+    import Link from '$lib/components/Link.svelte';
 
     let sponsorTiers = [
             {
@@ -68,6 +69,17 @@
 
 <PageHeader title="Partners & Sponsors" description="THE ORGANIZATIONS MAKING MMT POSSIBLE" />
 
+<Section>
+    <br /> <br />
+    <Heading text="Sponsors and Partners" size={4} textColor="#1B9AAA" />
+    <FlexBox>
+        <PanelBox width="60%" style="min-width: 360px;">
+            <p style="font-size: 1.2em;">Thank you to our sponsors and partners for supporting the Mustang Math Tournament and our vision of providing a fun, collaborative team competition to motivated and bright middle school students!<br><br>
+            Interested in sponsoring or partnering? Take a look at our <Link url="https://docs.google.com/presentation/d/1h2_5qUYBZybu6CEsQBB2FKEuYc0BitbsnBrLiRUCgJA/edit?usp=sharing" text="sponsorship information" />! You can contact us at <Link url="mailto:mustangmathtournament@gmail.com" text="mustangmathtournament@gmail.com" />.</p>
+        </PanelBox>
+    </FlexBox>
+</Section>
+
 {#each sponsorTiers as lvl}
     <Section>
         <br /> <br />
@@ -75,8 +87,10 @@
             <FlexBox wrap={true} align="stretch">
                 {#if lvl.sponsors.length > 0}
                     {#each lvl.sponsors as sponsor}
-                        <PanelBox borderRadius="20px" width="30%" style="max-width: 300px; align-items: center; display: flex;">
-                            <Image target="_blank" url={sponsor.url} alt={sponsor.alt} link={sponsor.link} />
+                        <PanelBox borderRadius="20px" width="40%" style="max-width: 300px;">
+                            <a target="_blank" href={sponsor.link} style="height: 100%; display: flex; align-items: center;">
+                                <img src={sponsor.url} alt={sponsor.alt} style="max-width: 100%;">
+                            </a>
                         </PanelBox>
                     {/each}
                 {:else}
