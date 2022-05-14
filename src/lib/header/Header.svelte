9d8279156aa95fe49c1c76679a806f6a9aec7074
Eesha-Jain <eesha.jain801@gmail.com>
2022-05-14 12:11:20 -0700

    fixed animation & nav bar

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,54 +1,12 @@
 <script>
-	import { page, navigating } from '$app/stores';
+	import { page } from '$app/stores';
 	import logo from './MMTLogo.png';
-    import { crossfade } from "svelte/transition";
-	import { onMount } from 'svelte';
-
-	let windowWidth;
-	let open = "none";
-	let change = 800;
-	
-	const debounce = (func, delay) => {
-		let timer;
-
-		return function () {
-			const context = this;
-			const args = arguments;
-			clearTimeout(timer);
-			timer = setTimeout(() => func.apply(context, args), delay);
-		};
+    import { slide, crossfade } from "svelte/transition";
+	let windowWidth = 0;
+	let showMobile = false;
+	function toggleMobile(){
+		showMobile = showMobile ? false : true;
 	};
-	
-	const setWindowWidth = () => {
-		windowWidth = `${window.innerWidth}px`;
-		openUpdate();
-	};
-
-	function onClickToggle() {
-		if (open == 'flex') { open = 'none';}
-		else {open = 'flex';}
-	}
-
-	function openUpdate() {
-		if (window.innerWidth < change) {
-			open = 'none';
-		} else {
-			open = "flex";
-		}
-	}
-
-	$: if($navigating) openUpdate();
-	
-	const debouncedSetWindowWidth = debounce(setWindowWidth, 300);
-	
-	onMount(() => {	
-		openUpdate();	
-		window.addEventListener('resize', debouncedSetWindowWidth);
-
-		return () => {
-			window.removeEventListener('resize', debouncedSetWindowWidth);
-		}
-	});
 
     const [send, receive] = crossfade({
         duration: 400
@@ -56,137 +14,183 @@
 
     const navPages = [
         {path: "/", text: "Home"},
-		{path: "/mmt-2022", text: "MMT 2022"},
+        {path: "/mmt-2022", text: "MMT 2022"},
         {path: "/our-team", text: "Our Team"},
 		{path: "/sponsors", text: "Sponsors"},
         {path: "/past-exams", text: "Past Exams"},
     ]
+
+    const HAMBURGER_BREAKPOINT = 940; // change if adding more pages
+    const MMT_BREAKPOINT = HAMBURGER_BREAKPOINT+310;
 </script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<header>
-	<ul class="tabs">
-		<li class="logo"><a href="/"><img src={logo} alt="mmtlogo" width="50px" height="50px" /></a></li>
-		
-		{#each navPages as navPage (navPage.path)}
-			<li class="tab" style="display: {open};">
-				<a sveltekit:prefetch href="{navPage.path}" class="textnav" class:active={$page.url.pathname === navPage.path}>
-					<span>{navPage.text}
-						{#if $page.url.pathname === navPage.path}
-						<div class="textunderline" in:receive|local out:send|local></div>
-						{/if}
-					</span>
-				</a>
-			</li>			
-		{/each}
+<svelte:window bind:innerWidth={windowWidth} />
 
-		<li on:click={onClickToggle} class="toggle"><i id="hamburger-icon" class="fa fa-bars" style="color:white;width:100%;height:100%;"></i></li>
-	</ul>
+<header>
+	<div class="corner">
+		<a href="/">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'} style="text-align: left; width: 100%; font-weight: bold; text-decoration: none;"><a sveltekit:prefetch href="/" style="font-size: 1.25rem;">
+				{#if windowWidth > MMT_BREAKPOINT}
+				Mustang Math Tournament
+				{:else}
+				MMT
+				{/if}
+			</a></li>
+			{#if windowWidth > HAMBURGER_BREAKPOINT || windowWidth===0}
+                {#each navPages as navPage (navPage.path)}
+                <li>
+                    <a sveltekit:prefetch href="{navPage.path}" class="textnav">
+                        <span>{navPage.text}
+                            {#if $page.url.pathname === navPage.path}
+                            <div class="textunderline" in:receive out:send></div>
+                            {/if}
+                        </span>
+                    </a>
+                </li>
+                {/each}
+			{:else}
+			<div on:click={toggleMobile} id="hamburger-div">
+			<i id="hamburger-icon" class="fa fa-bars"></i>
+			</div>
+			{/if}
+		</ul>
+	</nav>
 </header>
+{#if showMobile && windowWidth < HAMBURGER_BREAKPOINT}
+	<div id="hamburger-links" transition:slide|local={{ duration: 300 }}>
+        {#each navPages as navPage (navPage.path)}
+		<div class="exterior"><a on:click={toggleMobile} class:active={$page.url.pathname === navPage.path} sveltekit:prefetch href="{navPage.path}">{navPage.text}</a></div>
+		{/each}
+    </div>
+{/if}
 
 <style>
 	header {
-		background-color: #1c6825;
-		margin: 0;
-		padding: 0;
-		border: 1px solid transparent;
-		display: block;
-	}
-
-	.toggle {
-		position: absolute;
-		top: 25px;
-		right: 25px;
-	}
-
-	ul, li {
-		margin: 0;
-		padding: 0;
-	}
-
-	.logo {
-		flex: 1;
-		padding-left: 10px;
-		padding-top: 5px;
-		padding-bottom: 5px;
-	}
-
-	.tabs {
-		list-style-type: none;
 		display: flex;
-		flex-wrap: wrap;
+		justify-content: space-between;
+		background-color: #1c6825;
+	}
+
+	.corner {
+		width: 4em;
+		height: 4em;
+	}
+
+	.corner a {
+		display: flex;
 		align-items: center;
+		justify-content: left;
+		width: 100%;
+		height: 100%;
 	}
 
-	.tab {
-		float: right;
-		padding: 6px 20px;
-		margin: 5px;
-		transition: display 10s;
+	.corner img {
+		width: 3em;
+		height: 3em;
+		object-fit: contain;
 	}
 
-	.tab a {
-		text-decoration: none;
+	nav {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		list-style: none;
+		background: #1c6825;
+		background-size: contain;
+		width: 100%;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+		padding: 0 1em;
 		color: white;
+		font-size: 1rem;
 		text-transform: uppercase;
 		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+		width: fit-content;
 	}
 
-	.toggle {
-		display: none;
-	}
-
-	.textunderline {
-		height: 2px;
-		background-color: #FFF;
-		width: 100%;
-		position: absolute;
-		bottom: -2px;
-		left: 0;
-	}
-
-	.textnav>span {
+    .textnav>span {
         position: relative;
     }
+
+    .textunderline {
+        height: 2px;
+        background-color: #FFF;
+        width: 100%;
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+    }
+
+	.textnav {
+		width: max-content;
+	}
 
 	a:hover {
 		color: lightgreen;
 	}
 
-	@media screen and (max-width: 800px) {
-		.toggle {
-			display: flex;
-		}
+	#hamburger-icon {
+		margin-left: 40%;
+		margin-right: 40%;
+		margin-top: 40%;
+		margin-bottom: 40%;
+	}
 
-		.tab {
-			background-color: #65c083;
-			border: 1px solid #65c083;
-			padding: 2px;
-		}
-		
-		.tab, .tab a {
-			width: 100%;
-			display: flex;
-			justify-content: center;
-			margin: 0;
-		}
+	#hamburger-div {
+		width: 80px;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		color: white;
+	}
 
-		.tab a {
-			padding: 10px;
-			width: 25%;
-		}
+	#hamburger-links {
+		background-color: #65c083;
+		padding: 10px;
+	}
 
-		.tab a:hover {
-			color: white;
-		}
+	.exterior {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-		.active {
-			background-color: #5b8064;
-		}
+	.exterior .active {
+		background-color: #5b8064;
+		text-decoration: none;
+	}
 
-		.textunderline {
-			background-color: transparent;
-		}
+	#hamburger-links a {
+		padding: 10px;
+		text-align: center;
 	}
 </style>
