28d94c8b0510f613ed3c8b6e8276c6ac1bdc4e1e
Raymon Zhang <raymonzhang20@gmail.com>
2022-08-09 18:16:44 -0700

    extract socials link into separate component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
+	import SocialsLink from '$lib/components/SocialsLink.svelte';
 	import '../app.css';
     import { page } from "$app/stores";
 </script>
@@ -15,21 +16,11 @@
 	<footer>
 		<p><b>Contact at </b><a href="mailto:mustangmathtournament@gmail.com">mustangmathtournament@gmail.com</a><br /></p>
 		<div class="socials">
-			<a href="https://discord.gg/bYDDWxan5d" target="_blank" rel="noopener noreferrer" class="social-link">
-				<i class="fa-brands fa-discord"></i>
-			</a>
-			<a href="https://www.facebook.com/MustangMath" target="_blank" rel="noopener noreferrer" class="social-link">
-				<i class="fa-brands fa-facebook"></i>
-			</a>
-			<a href="https://www.instagram.com/mustangmath/" target="_blank" rel="noopener noreferrer" class="social-link">
-				<i class="fa-brands fa-instagram"></i>
-			</a>
-			<a href="https://www.linkedin.com/company/mustangmathtournament/" target="_blank" rel="noopener noreferrer" class="social-link">
-				<i class="fa-brands fa-linkedin"></i>
-			</a>
-			<a href="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" target="_blank" rel="noopener noreferrer" class="social-link aops">
-				AoPS
-			</a>
+			<SocialsLink url="https://discord.gg/bYDDWxan5d" icon="discord" />
+			<SocialsLink url="https://www.facebook.com/MustangMath" icon="facebook" />
+			<SocialsLink url="https://www.instagram.com/mustangmath/" icon="instagram" />
+			<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" icon="linkedin" />
+			<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" icon="AoPS" text />
 		</div>
 	</footer>
     {/if}
@@ -85,14 +76,5 @@
 		font-size: 1.5rem;
 		padding-top: 10px;
 		padding-bottom: 20px;
-	}
-
-	.social-link {
-		padding: 10px;
-	}
-
-	.aops {
-		font-size: 1.2rem;
-		font-weight: bold;
 	}
 </style>
