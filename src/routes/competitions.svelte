da2b44c50e35b03069053378f66e46cb6bdea283
Eesha-Jain <eesha.jain801@gmail.com>
2022-08-10 21:39:56 -0700

    integrated dictionary

--- src/routes/competitions.svelte
+++ src/routes/competitions.svelte
@@ -6,24 +6,16 @@
     import Button from '$lib/components/Button.svelte';
     import PageHeader from '$lib/components/PageHeader.svelte';
 
-    let background0 = "right-arrow.png";
-    let background1 = "right-arrow.png";
-
-    function toggleBackground0() {
-        if (background0 === "right-arrow.png") {
-            background0 = "right-arrow-shaded.png";
-        } else {
-            background0 = "right-arrow.png";
+    let competitions = [
+        {
+            title: "MMT 2023",
+            location: "mmt-2023"
+        },
+        {
+            title: "Individual",
+            location: "individual"
         }
-    }
-
-    function toggleBackground1() {
-        if (background1 === "right-arrow.png") {
-            background1 = "right-arrow-shaded.png";
-        } else {
-            background1 = "right-arrow.png";
-        }
-    }
+    ]
 </script>
 
 <svelte:head>
@@ -32,22 +24,15 @@
 
 <PageHeader title="Competitions" description="CHECK OUT OUR UPCOMING COMPETITIONS" button_url="https://contestdojo.com/" button_text="Register on ContestDojo!" button_id="registerOnContestDojo"/>
 <section style="padding: 30px;">
+    {#each competitions as competition, i}
     <PanelBox>
-        <div on:mouseenter={toggleBackground0} on:mouseleave={toggleBackground0}>
-            <a class="headerButton" href="/competitions/mmt-2023">
-                <p class="headerButton" id="MMT2023">MMT 2023</p>
-                <img height="25px" class="headerButton" src={background0} alt="Right-facing arrow icon" />
+        <div>
+            <a class="headerButton" href="/competitions/{competition.location}">
+                <p class="headerButton effect effect-1" id={competition.location}>{competition.title}</p>
             </a>
         </div>
     </PanelBox>
-    <PanelBox>
-        <div on:mouseenter={toggleBackground1} on:mouseleave={toggleBackground1}>
-            <a class="headerButton" href="/competitions/individual">
-                <p class="headerButton" id="individual">Individual</p>
-                <img height="25px" class="headerButton" src={background1} alt="Right-facing arrow icon" />
-            </a>
-        </div>
-    </PanelBox>
+    {/each}
 </section>
 
 <style>
@@ -70,4 +55,44 @@
     .headerButton:hover {
         text-decoration: none;
     }
+
+    .effect {
+		text-align: center;
+		display: inline-block;
+		position: relative;
+		text-decoration: none;
+		text-transform: capitalize;
+		width: 30%;
+		overflow: hidden;
+	}
+
+	/* effect-1 styles */
+
+	.effect.effect-1 {
+		transition: all 0.2s linear 0s;
+	}
+
+	.effect.effect-1:before {
+		content: '\f178';
+		font-family: FontAwesome;
+		position: absolute;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		right: 0;
+		top: 0;
+		opacity: 0;
+		height: 100%;
+		width: 40px;
+		transition: all 0.2s linear 0s;
+	}
+
+	.effect.effect-1:hover {
+		text-indent: -20px;
+	}
+
+	.effect.effect-1:hover:before {
+		opacity: 1;
+		text-indent: 0px;
+	}
 </style>
