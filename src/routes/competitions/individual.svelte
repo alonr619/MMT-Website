1eada12c77a2d5ace2abc877cfbe1c96e2c62249
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-09-01 12:12:23 -0700

    added dates for next competitions

--- src/routes/competitions/individual.svelte
+++ src/routes/competitions/individual.svelte
@@ -12,10 +12,15 @@
     let windowWidth;
 </script>
 <svelte:head>
-    <title>Individual</title>
+    <title>3MT</title>
 </svelte:head>
 
 <svelte:window bind:innerWidth={windowWidth} />
 
-<PageHeader title="Individual" description="More information coming soon..." button_url="https://contestdojo.com/" button_text="Register on ContestDojo!" button_id="registerOnContestDojo" />
-<br /> <br />
+<PageHeader title="Mustang Mini Math Tournament" description="Coming November 19th, 2022!" button_url="https://contestdojo.com/" button_text="Register on ContestDojo!" button_id="registerOnContestDojo" />
+<Heading text="The Individual Round" size=2.5 textColor="#1B9AAA" />
+<div style="margin-left: 10vw; margin-right: 10vw;">
+    <PanelBox>
+        <p style="font-size: 1.5em; text-align: center;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </PanelBox>
+</div>
