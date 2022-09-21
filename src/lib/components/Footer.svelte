76524877e6cefa3df184002bbafd43d59b4991b7
Alan Duan <fluffy_pentacorns@hotmail.com>
2022-09-21 16:33:56 -0700

    move footer to separate component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,55 @@
+<script>
+    import SocialsLink from '$lib/components/SocialsLink.svelte';
+</script>
+<footer>
+	<div class="socials">
+		<!--<button class="sign-up" on:click={() => {show = !show;}}>Sign Up for our Newsletter</button>-->
+		<SocialsLink url="mailto:mustangmathtournament@gmail.com" type="solid" icon="envelope" />
+		<SocialsLink url="https://discord.gg/bYDDWxan5d" type="brands" icon="discord" />
+		<SocialsLink url="https://www.facebook.com/MustangMath" type="brands" icon="facebook" />
+		<SocialsLink url="https://www.instagram.com/mustangmath/" type="brands" icon="instagram" />
+		<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" type="brands" icon="linkedin"/>
+		<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" type="text" icon="AoPS" text />
+	</div>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		top: 5px;
+		bottom: 0;
+		width: 100%;
+		text-align: center;
+		position: sticky;
+		z-index: 1;
+		min-height: 50px;
+		background-color: #1c6825;
+	}
+
+	footer p{
+		color: white;
+	}
+
+	footer a {
+		color: white;
+		opacity: 0.6;
+	}
+
+	footer a:hover{
+		text-decoration: underline;
+		opacity: 0.8;
+	}
+    
+	.socials {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.5rem;
+		padding-top: 10px;
+		padding-bottom: 8px;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
+    import Footer from '$lib/components/Footer.svelte';
 	import SocialsLink from '$lib/components/SocialsLink.svelte';
 	import '../app.css';
     import { page } from "$app/stores";
-  import Newsletter from '$lib/components/Newsletter.svelte';
+    import Newsletter from '$lib/components/Newsletter.svelte';
 
 	let show = false;
 </script>
@@ -14,28 +15,18 @@
 	<main>
 		<slot />
 		<Newsletter show={show} />
-        <aside>
-    			<SocialsLink url="mailto:mustangmathtournament@gmail.com" type="solid" icon="envelope" />
-    			<SocialsLink url="https://discord.gg/bYDDWxan5d" type="brands" icon="discord" />
-    			<SocialsLink url="https://www.facebook.com/MustangMath" type="brands" icon="facebook" />
-    			<SocialsLink url="https://www.instagram.com/mustangmath/" type="brands" icon="instagram" />
-    			<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" type="brands" icon="linkedin"/>
-    			<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" type="text" icon="AoPS" text />
-        </aside>
 	</main>
+    <aside>
+ 		<SocialsLink url="mailto:mustangmathtournament@gmail.com" type="solid" icon="envelope" />
+		<SocialsLink url="https://discord.gg/bYDDWxan5d" type="brands" icon="discord" />
+		<SocialsLink url="https://www.facebook.com/MustangMath" type="brands" icon="facebook" />
+ 		<SocialsLink url="https://www.instagram.com/mustangmath/" type="brands" icon="instagram" />
+    	<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" type="brands" icon="linkedin"/>
+    	<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" type="text" icon="AoPS" text />
+    </aside>
 
     {#if $page.url.pathname !== "/"}
-	<footer>
-		<div class="socials">
-			<!--<button class="sign-up" on:click={() => {show = !show;}}>Sign Up for our Newsletter</button>-->
-			<SocialsLink url="mailto:mustangmathtournament@gmail.com" type="solid" icon="envelope" />
-			<SocialsLink url="https://discord.gg/bYDDWxan5d" type="brands" icon="discord" />
-			<SocialsLink url="https://www.facebook.com/MustangMath" type="brands" icon="facebook" />
-			<SocialsLink url="https://www.instagram.com/mustangmath/" type="brands" icon="instagram" />
-			<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" type="brands" icon="linkedin"/>
-			<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" type="text" icon="AoPS" text />
-		</div>
-	</footer>
+        <Footer />
     {/if}
 </div>
 
@@ -69,46 +60,7 @@
 		min-height: 100vh;
 		box-sizing: border-box;
 	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		top: 5px;
-		bottom: 0;
-		width: 100%;
-		text-align: center;
-		position: sticky;
-		z-index: 1;
-		min-height: 50px;
-		background-color: #1c6825;
-	}
-
-	footer p{
-		color: white;
-	}
-
-	footer a {
-		color: white;
-		opacity: 0.6;
-	}
-
-	footer a:hover{
-		text-decoration: underline;
-		opacity: 0.8;
-	}
-
-	.socials {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-		font-size: 1.5rem;
-		padding-top: 10px;
-		padding-bottom: 8px;
-	}
-
+    
     aside {
         position: fixed;
         z-index: 2;
