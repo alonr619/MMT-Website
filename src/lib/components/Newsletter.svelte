55bc30b84e534c33c696f5d0bf4fb2cee465fab1
Eesha-Jain <eesha.jain801@gmail.com>
2022-09-21 14:56:47 -0700

    fix newsletter widget

--- src/lib/components/Newsletter.svelte
+++ src/lib/components/Newsletter.svelte
@@ -8,12 +8,11 @@
 <!-- Begin Mailchimp Signup Form -->
     <link href="//cdn-images.mailchimp.com/embedcode/classic-071822.css" rel="stylesheet" type="text/css">
     <div id="mc_embed_signup">
+        <div style="position: absolute; top: 15px; right: 15px;"><i class="fa-solid fa-x" style="color: gray;cursor:pointer;" on:click={() => {show = !show;}}></i></div>
         <form action="https://mustangmath.us14.list-manage.com/subscribe/post?u=c80101147f7690b2cd88056c7&amp;id=10f0771adf&amp;f_id=007a87e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
-                <br />
-            <div style="display: flex;align-items: center;justify-content: space-between">
+            <div>
                 <h2><strong>Join Mustang Math Newsletter</strong></h2>
-                <i class="fa-solid fa-x" style="color: gray;" on:click={() => {show = !show;}}></i>
             </div>
     <div class="mc-field-group">
         <label for="mce-FNAME">First Name</label>
@@ -31,15 +30,15 @@
         <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required>
         <span id="mce-EMAIL-HELPERTEXT" class="helper_text"></span>
     </div>
-    <div style="display: flex; align-items: center; justify-content: center">
-    <div id="mce-responses" class="clear foot">
+    <div>
+    <div id="mce-responses" class="clear foot" style="width: 0%;">
         <div class="response" id="mce-error-response" style="display:none"></div>
         <div class="response" id="mce-success-response" style="display:none"></div>
     </div>
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c80101147f7690b2cd88056c7_10f0771adf" tabindex="-1" value=""></div>
         <div class="optionalParent">
             <div class="clear foot">
-                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
             </div>
         </div>
     </div>
@@ -52,15 +51,14 @@
 
 <style>
     .surround {
-        background-color: rgba(0, 0, 0, 0.6);
+        background-color: rgba(0, 0, 0, 0.4);
         display: flex;
         align-items: center;
         justify-content: center;
-        position: absolute;
-        top: 0;
-        left: 0;
+        position: fixed;
+        top: 0px;
         width: 100%;
-        height: 100%;
+        height: 100vh;
         z-index: 100;
     }
 
@@ -68,10 +66,28 @@
         background:#fff; 
         clear:left; 
         width:400px;
+        padding: 5px;
+        padding-right: 0;
+        border-radius: 15px;
+        position: relative;
+        grid-template-columns: 4fr;
+    }
+
+    #mc_embed_signup .foot {
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
     .button {
         width: 100% !important;
-        background-color: #1c6825 !important;
+        margin: 0 !important;
+        background-color: #2c9c39 !important;
+    }
+
+    #mc_embed_signup_scroll {
+        margin: 0 !important;
+        width: 100%;
+        padding: 0 !important;
     }
 </style>
