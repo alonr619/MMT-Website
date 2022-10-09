d7e7e50076a59074e32f73b24397ccf3c0e08a13
Wattle2 <108093525+Wattle2@users.noreply.github.com>
2022-10-08 22:24:11 -0700

    added __error.svelte

---
+++ src/routes/__error.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export function load({ error, status }) {
+		return {
+			props: { error, status }
+		};
+	}
+</script>
+
+<script>
+	/** @type {number} */
+	export let status;
+
+	/** @type {Error & {frame?: string} & {loc?: object}} */
+	export let error;
+
+	import PageHeader from '$lib/components/PageHeader.svelte';
+</script>
+<html lang="">
+	
+<PageHeader title="Error: {status}" description="{error.message}" button_url="/" button_text= "Come back to charted territory!" button_id="homeButton"/> 
+	
+
+</html>
+<style>
+	
+</style>
+<!-- TODO figure out what to do with frames/stacktraces in prod -->
+<!-- frame is populated by Svelte in its CompileError and is a Rollup/Vite convention -->
+<!--
+{#if error.frame}
+	<pre>{error.frame}</pre>
+{/if}
+{#if error.stack}
+	<pre>{error.stack}</pre>
+{/if} 
+-->
