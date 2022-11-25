35e014a2b2098e32aec63c4ec9ea04a022087557
Eesha-Jain <eesha.jain801@gmail.com>
2022-11-24 22:41:11 -0800

    redirect

---
+++ src/routes/3MTOpeningCeremony.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+    export async function load() {
+        return {
+            status: 302,
+            redirect: "https://us06web.zoom.us/j/84483247251?pwd=UVMrZ1k2RnFuTEkrUUd4NVdyemMwZz09#success"
+        };
+    }
+</script>
