b63efc4be7d6b70b37025d29ed05060f5493aa09
Eesha-Jain <eesha.jain801@gmail.com>
2023-01-10 20:19:34 -0800

    fixing - arpit

--- src/routes/redirect.svelte
+++ src/routes/redirect.svelte
@@ -1,8 +1,7 @@
 <script context="module">
     export async function load({ url }) {
         const link = url.searchParams.get('link');
-        const get = url.searchParams.get('get').replace("%26", "&") + "&link=" + link;
-
+        const get = url.searchParams.get('get').replace("%26", "&") + "&link=" + link.replace("&", "%26");
 
         if (get) {
             await fetch(get, {
