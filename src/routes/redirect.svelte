de9ec8283ae8a11f5d9b8e233030f0fdd1b7baeb
Eesha-Jain <eesha.jain801@gmail.com>
2023-01-10 20:15:39 -0800

    fun redirect things yay

---
+++ src/routes/redirect.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export async function load({ url }) {
+        const link = url.searchParams.get('link');
+        const get = url.searchParams.get('get').replace("%26", "&") + "&link=" + link;
+
+
+        if (get) {
+            await fetch(get, {
+                method: 'GET'
+            });
+        }
+
+        return {
+            status: 302,
+            redirect: link
+        };
+    }
+</script>
