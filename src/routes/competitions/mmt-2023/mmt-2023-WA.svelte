8f3780e708717b22e49f80bc51665b55fc2eb2bb
tyranasaurus <arpitr@stanford.edu>
2023-03-16 07:29:34 -0700

    extend early

--- src/routes/competitions/mmt-2023/mmt-2023-WA.svelte
+++ src/routes/competitions/mmt-2023/mmt-2023-WA.svelte
@@ -69,7 +69,7 @@
         <p>
             <strong>TEAM SIZE:</strong> 3-4* <br />
             <strong>LOCATION:</strong> Mill Creek Middle School (Kent, Washington)<br />
-            <strong>EARLY HORSE COST:</strong> $10 per participant (Register before <strong>March 14th, 2023</strong> to receive the early horse price!)<br />
+            <strong>EARLY HORSE COST:</strong> $10 per participant (Register before <strong>March 17th, 2023</strong> to receive the early horse price!)<br />
             <strong>NORMAL COST:</strong> $15 per participant (Register before <strong>April 22nd, 2023</strong> to participate)<br /> <br />
             <a href="https://docs.google.com/document/d/1FRL4DLS2RVbbMRKe2r0t7K2Rhjabwr45TdgM0ISDKnQ/edit?usp=sharing" target="_blank"><strong>CLICK HERE FOR REGISTRATION INSTRUCTIONS</strong></a>
             <br /><br />
