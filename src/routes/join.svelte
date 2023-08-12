424b309a067f43b6fc8194573189cc6febb81c28
Eesha-Jain <eesha.jain801@gmail.com>
2023-08-12 14:45:10 -0700

    modified text

--- src/routes/join.svelte
+++ src/routes/join.svelte
@@ -22,7 +22,7 @@
 
 <div style="margin-left: 10vw; margin-right: 10vw;">
     <PanelBox>
-        <p style="font-size: 1.5em; text-align: center;"><strong>Notice:</strong><br /> We are mainly recruiting for Tournament Development from specific regions. However, you may get the opportunity to join other teams.</p>
+        <p style="font-size: 1.5em; text-align: center;"><strong>Notice:</strong><br /> We are mainly recruiting for Tournament Development from Massachusetts, Illinois, and British Columbia. However, you may get the opportunity to join other teams.</p>
     </PanelBox>
 </div> <br />
 
@@ -44,7 +44,7 @@
 <Heading text="In-Person Locations" size={2.5} textColor="#1B9AAA" />
 <div style="margin-left: 10vw; margin-right: 10vw;">
     <PanelBox>
-        <p style="font-size: 1.5em; text-align: center;">For 2023-2024, we are looking to expand our in-person offerings to include Washington (Seattle), California (Bay), Illinois (Chicago), and Massachusetts (Boston). These locations would be responsible for running an in-person location of MMT 2023 to the students from their state and nearby areas, and will also receive support from Mustang Math to run other similar events. If you're from these areas and want to help give a wonderful contest experience to your local middle school students, highly consider joining!</p>
+        <p style="font-size: 1.5em; text-align: center;">For 2023-2024, we are looking to expand our in-person offerings to include Washington (Seattle), California (Bay), Illinois (Chicago), British Columbia (Vancouver), and Massachusetts (Boston). These locations would be responsible for running an in-person location of MMT 2023 to the students from their state and nearby areas, and will also receive support from Mustang Math to run other similar events. If you're from these areas and want to help give a wonderful contest experience to your local middle school students, highly consider joining!</p>
     </PanelBox>
 </div> <br />
 
