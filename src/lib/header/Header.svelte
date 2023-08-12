420e8e4e92c5c8ef8f177ab94a6b268bf3e23d20
Eesha-Jain <eesha.jain801@gmail.com>
2023-08-12 14:22:09 -0700

    updated join

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -24,7 +24,7 @@
 
 	const navPages = [
 		{path: "/", text: "Home", hasSubPages: false},
-		{path: "/join", text: "Join Our Team", hasSubPages: false},
+		{path: "/join", text: "Join Us", hasSubPages: false},
 		{	path: "/competitions", 
 			text: "Competitions", 
 			hasSubPages: true,
