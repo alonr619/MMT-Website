83e469acdc5aa7de2672935cd453490ab8b3c8c2
TheDhruvJena <thedhruvjena@gmail.com>
2023-08-25 15:33:37 -0700

    ADA Fixing any low contrast areas
    Fixed all the buttons (still need the help on the first page)
    Added a hidden span of text after each font awesome icon

--- src/lib/components/SocialsLink.svelte
+++ src/lib/components/SocialsLink.svelte
@@ -1,38 +1,49 @@
 <script>
-    export let url;
-    export let type;
-    export let icon;
-    export let text = false;
+  export let url;
+  export let type;
+  export let icon;
+  export let text = false;
 </script>
 
-<a href={url} target="_blank" rel="noopener noreferrer" class="social-link" class:text>
-    {#if text}
-        {icon}
-    {:else}
-        <i class="fa-{type} fa-{icon}"></i>
-    {/if}
+<a
+  href={url}
+  target="_blank"
+  rel="noopener noreferrer"
+  class="social-link"
+  class:text
+>
+  {#if text}
+    {icon}
+  {:else}
+    <i class="fa-{type} fa-{icon}" />
+    <span class="hidden">{type} {icon}</span>
+  {/if}
 </a>
 
 <style>
+  .social-link {
+    padding: 10px;
+    color: white;
+    opacity: 0.6;
+  }
+
+  @media screen and (max-width: 900px) {
     .social-link {
-        padding: 10px;
-		color: white;
-		opacity: 0.6;
+      padding: 4px;
     }
+  }
 
-    @media screen and (max-width: 900px) {
-        .social-link {
-            padding: 4px;
-        }
-    }
+  .social-link:hover {
+    text-decoration: underline;
+    opacity: 0.8;
+  }
 
-    .social-link:hover {
-        text-decoration: underline;
-        opacity: 0.8;
-    }
+  .text {
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
 
-    .text {
-        font-size: 1.2rem;
-        font-weight: bold;
-    }
+  .hidden {
+    display: none;
+  }
 </style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,94 +1,135 @@
 <script>
-	import Header from '$lib/header/Header.svelte';
-	import SocialsLink from '$lib/components/SocialsLink.svelte';
-	import '../app.css';
-    import { page } from "$app/stores";
-	import Newsletter from '$lib/components/Newsletter.svelte';
-	import { user } from '$lib/store';
-	import { onMount } from "svelte";
+  import Header from "$lib/header/Header.svelte";
+  import SocialsLink from "$lib/components/SocialsLink.svelte";
+  import "../app.css";
+  import { page } from "$app/stores";
+  import Newsletter from "$lib/components/Newsletter.svelte";
+  import { user } from "$lib/store";
+  import { onMount } from "svelte";
 
-	let show = false;
+  let show = false;
 
-	onMount(() => {
-        user.subscribe((u) => (show = u));
-    })
+  onMount(() => {
+    user.subscribe((u) => (show = u));
+  });
 </script>
 
 <div class="all-container">
-    <Header />
+  <Header />
 
-	<main>
-		<slot />
-		<Newsletter show={show} />
-	</main>
+  <main>
+    <slot />
+    <Newsletter {show} />
+  </main>
 
-    {#if $page.url.pathname !== "/"}
-	<footer>
-		<div class="socials">
-			<button class="sign-up" on:click={() => {show = !show;}}><i class="fa-regular fa-newspaper"></i> Signup</button>
-			<div class="divider"></div>
-			<SocialsLink url="mailto:contact@mustangmath.com" type="solid" icon="envelope" />
-			<SocialsLink url="https://discord.gg/bYDDWxan5d" type="brands" icon="discord" />
-			<SocialsLink url="https://www.facebook.com/MustangMath" type="brands" icon="facebook" />
-			<SocialsLink url="https://www.instagram.com/mustangmath/" type="brands" icon="instagram" />
-			<SocialsLink url="https://www.linkedin.com/company/mustangmathtournament/" type="brands" icon="linkedin"/>
-			<SocialsLink url="https://www.youtube.com/channel/UCCCk-s3dHatOKfwiC5yJBAA" type="brands" icon="youtube"/>
-			<SocialsLink url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament" type="text" icon="AoPS" text />
-		</div>
-	</footer>
-    {/if}
+  {#if $page.url.pathname !== "/"}
+    <footer>
+      <div class="socials">
+        <button
+          class="sign-up"
+          on:click={() => {
+            show = !show;
+          }}
+          ><i class="fa-regular fa-newspaper" /><span class="hidden"
+            >Newsletter Icon</span
+          > Signup</button
+        >
+        <div class="divider" />
+        <SocialsLink
+          url="mailto:contact@mustangmath.com"
+          type="solid"
+          icon="envelope"
+        />
+        <SocialsLink
+          url="https://discord.gg/bYDDWxan5d"
+          type="brands"
+          icon="discord"
+        />
+        <SocialsLink
+          url="https://www.facebook.com/MustangMath"
+          type="brands"
+          icon="facebook"
+        />
+        <SocialsLink
+          url="https://www.instagram.com/mustangmath/"
+          type="brands"
+          icon="instagram"
+        />
+        <SocialsLink
+          url="https://www.linkedin.com/company/mustangmathtournament/"
+          type="brands"
+          icon="linkedin"
+        />
+        <SocialsLink
+          url="https://www.youtube.com/channel/UCCCk-s3dHatOKfwiC5yJBAA"
+          type="brands"
+          icon="youtube"
+        />
+        <SocialsLink
+          url="https://artofproblemsolving.com/wiki/index.php/Mustang_Math_Tournament"
+          type="text"
+          icon="AoPS"
+          text
+        />
+      </div>
+    </footer>
+  {/if}
 </div>
 
 <style>
-	.divider {
-		width: 2px;
-		background-color: white;
-		height: 50px;
-		margin-left: 10px;
-	}
+  .divider {
+    width: 2px;
+    background-color: white;
+    height: 50px;
+    margin-left: 10px;
+  }
 
-	@media screen and (max-width: 900px) {
-		.divider {
-			margin-right: 5px;
-		}
-	}
+  @media screen and (max-width: 900px) {
+    .divider {
+      margin-right: 5px;
+    }
+  }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		width: 100%;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 
-	.all-container {
-		position: relative;
-		min-height: 100vh;
-		box-sizing: border-box;
-	}
+  .all-container {
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+  }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		bottom: 0;
-		width: 100%;
-		text-align: center;
-		position: sticky;
-		z-index: 2;
-		min-height: 50px;
-		background-color: #1c6825;
-	}
+  footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    position: sticky;
+    z-index: 2;
+    min-height: 50px;
+    background-color: #1c6825;
+  }
 
-	.socials {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content: center;
-		font-size: 1.5rem;
-		padding-top: 10px;
-		padding-bottom: 8px;
-	}
+  .socials {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    padding-top: 10px;
+    padding-bottom: 8px;
+  }
+
+  .hidden {
+    display: none;
+  }
 </style>
