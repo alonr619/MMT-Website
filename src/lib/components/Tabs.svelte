33d5b934adaf559e09f12fbb4581deef739938dc
TheDhruvJena <thedhruvjena@gmail.com>
2023-09-07 21:21:24 -0700

    Our Team Page Updated Tabs

--- src/lib/components/Tabs.svelte
+++ src/lib/components/Tabs.svelte
@@ -1,59 +1,75 @@
 <script>
-    export let items = [];
-    export let activeTabValue = 1;
-  
-    const handleClick = tabValue => () => (activeTabValue = tabValue);
-  </script>
-  
-  <ul>
-  {#each items as item}
-      <li class={activeTabValue === item.value ? 'active' : ''} style="background-color: {item.hex}">
-          <span on:click={handleClick(item.value)}>{item.label}</span>
+  export let items = [];
+  export let activeTabValue = 1;
+
+  const handleClick = (tabValue) => () => (activeTabValue = tabValue);
+</script>
+
+<ul>
+  <div class="rolebar">
+    {#each items as item}
+      <li
+        class={activeTabValue === item.value ? "active" : ""}
+        style="background-color: {item.hex}"
+      >
+        <span on:click={handleClick(item.value)}>{item.label}</span>
       </li>
-  {/each}
-  </ul>
-  {#each items as item}
-      {#if activeTabValue == item.value}
-      <div class="box" style="background-color: {item.hex}">
-          <slot {item} />
-      </div>
-      {/if}
-  {/each}
-  <style>
-      .box {
-          margin-bottom: 10px;
-          padding: 40px;
-          border: 1px solid #dee2e6;
-      border-radius: 0 0 .5rem .5rem;
-      border-top: 0;
-      }
-    ul {
-      display: flex;
-      flex-wrap: wrap;
-      padding-left: 0;
-      margin-bottom: 0;
-      list-style: none;
-    }
-      li {
-          margin-bottom: -1px;
-      }
-  
-    span {
-      border: 1px solid transparent;
-      border-top-left-radius: 0.25rem;
-      border-top-right-radius: 0.25rem;
-      display: block;
-      padding: 0.5rem 1rem;
-      cursor: pointer;
-    }
-  
-    span:hover {
-      border-color: #e9ecef #e9ecef #dee2e6;
-    }
-  
-    li.active > span {
-      color: #495057;
-      background-color: #fff;
-      border-color: #dee2e6 #dee2e6 #fff;
-    }
-  </style>
+    {/each}
+  </div>
+</ul>
+{#each items as item}
+  {#if activeTabValue == item.value}
+    <div class="box" style="background-color: {item.hex}">
+      <slot {item} />
+    </div>
+  {/if}
+{/each}
+
+<style>
+  .box {
+    margin-bottom: 10px;
+    border: 1px solid #dee2e6;
+    border-radius: 0 0 0.5rem 0.5rem;
+    border-top: 0;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding-left: 0;
+    margin-bottom: 0;
+    list-style: none;
+    width: 100%;
+  }
+  .rolebar {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    padding-left: 10px;
+  }
+  li {
+    margin-bottom: -11px;
+    border-radius: 17px 17px 0 0;
+    margin-right: 3px;
+  }
+
+  span {
+    border: 1px solid transparent;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+    display: block;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+  }
+
+  span:hover {
+    border-color: #e9ecef #e9ecef #dee2e6;
+  }
+
+  li.active > span {
+    color: #495057;
+    background-color: #fff;
+    border-color: #dee2e6 #dee2e6 #fff;
+    border-radius: 17px 17px 0 0;
+    font-size: 1.2em;
+  }
+</style>
--- src/routes/our-team.svelte
+++ src/routes/our-team.svelte
@@ -16,46 +16,14 @@
 
   // List of tab items with labels, values and assigned components
   let items = [
-    { label: "All Members",
-      role: "all",
-      value: 1,
-      hex: "#d3dde3"
-		},
-    { label: "Community Engagement",
-      role: "ce",
-      value: 2,
-      hex: "#d7efcb"
-		},
-    { label: "Curriculum Development",
-      role: "cd",
-      value: 3,
-      hex: "#cbefdf"
-		},
-    { label: "Design",
-      role: "d",
-      value: 4,
-      hex: "#cbe1ef"
-		},
-    { label: "Problem Writing",
-      role: "pw",
-      value: 5,
-      hex: "#d5cbef"
-		},
-    { label: "Technology",
-      role: "t",
-      value: 6,
-      hex: "#efcbeb"
-		},
-    { label: "Tournament Development",
-      role: "td",
-      value: 7,
-      hex: "#efcbcc"
-		},
-    { label: "Video Production",
-      role: "vp",
-      value: 8,
-      hex: "#efe9cb"
-		}
+    { label: "All Members", role: "all", value: 1, hex: "#d3dde3" },
+    { label: "Community Engagement", role: "ce", value: 2, hex: "#d7efcb" },
+    { label: "Curriculum Development", role: "cd", value: 3, hex: "#cbefdf" },
+    { label: "Design", role: "d", value: 4, hex: "#cbe1ef" },
+    { label: "Problem Writing", role: "pw", value: 5, hex: "#d5cbef" },
+    { label: "Technology", role: "t", value: 6, hex: "#efcbeb" },
+    { label: "Tournament Development", role: "td", value: 7, hex: "#efcbcc" },
+    { label: "Video Production", role: "vp", value: 8, hex: "#efe9cb" },
   ];
 
   let roles = {
@@ -86,31 +54,29 @@
     var usePound = false;
 
     if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
+      col = col.slice(1);
+      usePound = true;
     }
 
-    var num = parseInt(col,16);
+    var num = parseInt(col, 16);
 
     var r = (num >> 16) + amt;
 
     if (r > 255) r = 255;
-    else if  (r < 0) r = 0;
+    else if (r < 0) r = 0;
 
-    var b = ((num >> 8) & 0x00FF) + amt;
+    var b = ((num >> 8) & 0x00ff) + amt;
 
     if (b > 255) b = 255;
-    else if  (b < 0) b = 0;
+    else if (b < 0) b = 0;
 
-    var g = (num & 0x0000FF) + amt;
+    var g = (num & 0x0000ff) + amt;
 
     if (g > 255) g = 255;
     else if (g < 0) g = 0;
 
-    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-
+    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
   }
-
 </script>
 
 <svelte:head>
@@ -134,7 +100,11 @@
   <Tabs {items} let:item={tab}>
     <div class="tab">
       <PanelBox style="background-color: {tab.hex}; opacity: 1">
-        <Heading text={tab.label} size={3} textColor={LightenDarkenColor(tab.hex, -120)} />
+        <Heading
+          text={tab.label}
+          size={3}
+          textColor={LightenDarkenColor(tab.hex, -120)}
+        />
         <FlexBox wrap={true}>
           {#each displayedMembers as Member}
             {#if tab.role === "all"}
@@ -181,11 +151,8 @@
           {/each}
         </FlexBox>
       </PanelBox>
-      
     </div>
   </Tabs>
-
-  
 
   <h1>Other Contributors</h1>
   <div style="display: flex; justify-content: center; align-items: center;">
