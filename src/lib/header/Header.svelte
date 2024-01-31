549f2efc0741be7258e3d310c46d9c59d82d7c57
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2024-01-30 20:29:06 -0800

    state pages

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -30,7 +30,7 @@
       hasSubPages: true,
       index: 0,
       subPages: [
-        { path: "/competitions/3mt-2023", text: "3MT 2023"},
+        { path: "/competitions/mmt-2024", text: "MMT 2024"},
         { path: "/competitions/past", text: "Past" },
       ],
     },
